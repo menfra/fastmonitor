d8a4f95aeda2b34708b3fcc525f0a23929640c3d
Frank Mensah <33913399+menfra@users.noreply.github.com>
2024-11-12 20:17:03 +0100

    [DEV545] TLS Update

---
+++ FastMonitor/Configurations/FastMonitorOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastMonitor.Configurations
+{
+   public class FastMonitorOptions
+    {
+        public bool RequireRoleBasedAccess { get; set; } = true;
+        public bool LogAllAccessAttempts { get; set; } = true;
+    }
+}
---
+++ FastMonitor/Logic/FastMonitor.cs
@@ -0,0 +1,21 @@
+﻿using FastMonitor.Model;
+using System;
+
+namespace FastMonitor.Logic
+{
+    public class FastMonitorService : IFastMonitor
+    {
+        public bool CheckAccess(User user, int dataId)
+        {
+            // Add logic to verify if user has permissions for dataId
+            // Replace this with actual authorization logic
+            return user.HasPermission(dataId);
+        }
+
+        public void LogAccessAttempt(User user, int dataId)
+        {
+            // Log access attempt for auditing
+            Console.WriteLine($"User {user.Id} accessed data {dataId} at {DateTime.UtcNow}");
+        }
+    }
+}
